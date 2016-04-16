eaf5d9146deef7c7c34ef4e82a0a019491b0a1ba
nshaheen <xxnickflick22xx@Nicholass-MacBook-Pro.local>
2016-04-16 19:33:39 -0400

    login button links to loginplay.php

--- index.php
+++ index.php
@@ -1,56 +1,61 @@
 <!DOCTYPE html>
 
 <html lang="en">
+
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>NüL</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+    <title>NüL</title>
 
-  <!-- CSS  -->
-  <link href="css/index.css" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <!-- CSS  -->
+    <link href="css/index.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
 </head>
+
 <body>
-  <nav class="light-blue lighten-1" role="navigation">
-    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="#">Navbar Link</a></li>
-      </ul>
+    <nav class="light-blue lighten-1" role="navigation">
+        <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="#">Navbar Link</a></li>
+            </ul>
 
-      <ul id="nav-mobile" class="side-nav">
-        <li><a href="#">Navbar Link</a></li>
-      </ul>
-      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+            <ul id="nav-mobile" class="side-nav">
+                <li><a href="#">Navbar Link</a>
+                </li>
+            </ul>
+            <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+        </div>
+    </nav>
+    <div class="section no-pad-bot" id="index-banner">
+        <div class="container">
+            <br>
+            <br>
+            <h1 class="header center orange-text">NüL5.0 Login</h1>
+            <div class="row center">
+                <h5 class="header col s12 white-text">Presenting, the best game ever!</h5>
+            </div>
+            <div class="row center">
+                <a href="http://playcanvas.com/editor/scene/424681/launch" id="download-button" class="btn-large waves-effect waves-light orange">Demo</a>
+            </div>
+            <br>
+            <br>
+
+        </div>
     </div>
-  </nav>
-  <div class="section no-pad-bot" id="index-banner">
+
+
+
     <div class="container">
-      <br><br>
-      <h1 class="header center orange-text">NüL5.0 Login</h1>
-      <div class="row center">
-        <h5 class="header col s12 white-text">Presenting, the best game ever!</h5>
-      </div>
-      <div class="row center">
-        <a href="http://playcanvas.com/editor/scene/424681/launch" id="download-button" class="btn-large waves-effect waves-light orange">Demo</a>
-      </div>
-      <br><br>
+        <div class="section">
 
-    </div>
-  </div>
-
-
-
-  <div class="container">
-    <div class="section">
-
-      <!--   Icon Section   -->
-      <div class="row">
-        <div class="col s12 m4">
-          <div class="icon-block">
-            <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
-            <h5 class="center">
+            <!--   Icon Section   -->
+            <div class="row">
+                <div class="col s12 m4">
+                    <div class="icon-block">
+                        <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
+                        <h5 class="center">
 
 			<div class="row">
     <form class="col s12">
@@ -80,28 +85,28 @@
       </div>
     </form>
   </div>
-			 <id="download-button" class="btn-large waves-effect waves-light orange">Login</a></h5>
+			 <a href="loginplay.php" id="download-button" class="btn-large waves-effect waves-light orange">Login</a>
 
-            <p class="light">Login to play!</p>
-          </div>
-        </div>
-
-
+                        <p class="light">Login to play!</p>
+                    </div>
+                </div>
 
 
-        <div class="col s12 m4">
-          <div class="icon-block">
-            <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
-            <h5 class="center"><a href="http://playcanvas.com/editor/scene/424681/launch" id="download-button" class="btn-large waves-effect waves-light orange">High Scores</a></h5>
 
-            <p class="light">  Where do you rank?</p>
-          </div>
-        </div>
 
-        <div class="col s12 m4">
-          <div class="icon-block">
-            <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
-            <h5 class="center">
+                <div class="col s12 m4">
+                    <div class="icon-block">
+                        <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
+                        <h5 class="center"><a href="http://playcanvas.com/editor/scene/424681/launch" id="download-button" class="btn-large waves-effect waves-light orange">High Scores</a></h5>
+
+                        <p class="light"> Where do you rank?</p>
+                    </div>
+                </div>
+
+                <div class="col s12 m4">
+                    <div class="icon-block">
+                        <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
+                        <h5 class="center">
 
 
 
@@ -147,57 +152,64 @@
 		 <id="download-button" class="btn-large waves-effect waves-light orange">Register</a></h5>
 
 
-            <p class="light">   Don't have an account? No problem! Register today</p>
-          </div>
-        </div>
-      </div>
-
-    </div>
-    <br><br>
-
-    <div class="section">
-
-    </div>
-  </div>
-
-  <footer class="page-footer orange">
-    <div class="container">
-      <div class="row">
-        <div class="col l6 s12">
-          <h5 class="white-text"> Bio</h5>
-          <p class="grey-text text-lighten-4"> Join oZ5.0 on his grand escape! NüL is the next big, exciting puzzle platformer to hit stores and web consoles to date</p>
-
+                        <p class="light"> Don't have an account? No problem! Register today</p>
+                    </div>
+                </div>
+            </div>
 
         </div>
-        <div class="col l3 s12">
-          <h5 class="white-text">Settings</h5>
-          <ul>
-            <li><a class="white-text" href="#!">Subtitles</a></li>
-            <li><a class="white-text" href="#!">Audio</a></li>
-            <li><a class="white-text" href="#!">Gameplay</a></li>
-          </ul>
+        <br>
+        <br>
+
+        <div class="section">
+
         </div>
-        <div class="col l3 s12">
-          <h5 class="white-text">Connect</h5>
-          <ul>
-            <li><a class="white-text" href="#!">Email Us</a></li>
-            <li><a class="white-text" href="#!">Github</a></li>
-          </ul>
+    </div>
+
+    <footer class="page-footer orange">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text"> Bio</h5>
+                    <p class="grey-text text-lighten-4"> Join oZ5.0 on his grand escape! NüL is the next big, exciting puzzle platformer to hit stores and web consoles to date</p>
+
+
+                </div>
+                <div class="col l3 s12">
+                    <h5 class="white-text">Settings</h5>
+                    <ul>
+                        <li><a class="white-text" href="#!">Subtitles</a>
+                        </li>
+                        <li><a class="white-text" href="#!">Audio</a>
+                        </li>
+                        <li><a class="white-text" href="#!">Gameplay</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col l3 s12">
+                    <h5 class="white-text">Connect</h5>
+                    <ul>
+                        <li><a class="white-text" href="#!">Email Us</a>
+                        </li>
+                        <li><a class="white-text" href="#!">Github</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="footer-copyright">
-      <div class="container">
-      Brought to you by Nick and Rachel ;)
-      </div>
-    </div>
-  </footer>
+        <div class="footer-copyright">
+            <div class="container">
+                Brought to you by Nick and Rachel ;)
+            </div>
+        </div>
+    </footer>
 
 
-  <!--  Scripts-->
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="js/materialize.js"></script>
-  <script src="js/init.js"></script>
+    <!--  Scripts-->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="js/materialize.js"></script>
+    <script src="js/init.js"></script>
 
-  </body>
+</body>
+
 </html>
