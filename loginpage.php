541b7329f3e2618737a53aec74c6eb9ae19a6707
RPappalardo <Rachel.Pappalardo001@mymdc.net>
2016-04-17 21:42:17 -0400

    Added pictures

--- loginpage.php
+++ loginpage.php
@@ -17,7 +17,7 @@
 
 <body>
     <nav class="light-blue lighten-1" role="navigation">
-        <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
+        <div class="nav-wrapper container"><img width="40" src="bigrobologo.jpg">
             <ul class="right hide-on-med-and-down">
                 <li><a href="#">Navbar Link</a></li>
             </ul>
