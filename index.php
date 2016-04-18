066b36dceddcfdfb4f89f12bf4647683a25e5049
RPappalardo <Rachel.Pappalardo001@mymdc.net>
2016-04-17 21:57:34 -0400

    Removed Mobile Menu Name for Web Browsing View

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     <nav class="light-blue lighten-1" role="navigation">
         <div class="nav-wrapper container">
             <ul class="right hide-on-med-and-down">
-                <li><a href="#">Menu</a></li>
+                <li><a href="#"></a></li>
             </ul>
 
             <ul id="nav-mobile" class="side-nav">
