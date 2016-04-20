b4961cfc3d050ae416aef6f7ea2d0f80425a5e20
RPappalardo <Rachel.Pappalardo001@mymdc.net>
2016-04-20 01:21:47 -0400

    Added a Contact Us hyperlink to "Contact Us"

---
+++ contactus.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+  <title>NüL</title>
+
+  <!-- CSS  -->
+  <link href="css/index.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
+</head>
+
+
+
+
+<body>
+
+
+  <nav class="amber" role="navigation">
+      <div class="nav-wrapper container">
+          <ul class="right hide-on-med-and-down">
+              <li>
+                <a href="index.php">Home</a>
+              </li>
+          </ul>
+
+          <ul id="nav-mobile" class="side-nav">
+              <li><a href="index.php">Home</a>
+              </li>
+              <li><a href="loginpage.php">Login</a>
+              </li>
+              <li><a href="registerpage.php">Register</a>
+              </li>
+              <li><a href="http://playcanvas.com/editor/scene/424681/launch">Demo</a>
+              </li>
+          </ul>
+          <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+      </div>
+  </nav>
+  <div class="section no-pad-bot" id="index-banner">
+      <div class="container">
+          <br>
+          <br>
+          <h1 class="header center orange-text">NüL5.0 Login</h1>
+          <br>
+          <div class="row center">
+
+          </div>
+          <div class="row center">
+              <a href="http://playcanvas.com/editor/scene/424681/launch" id="download-button" class="btn-large waves-effect waves-light orange">Demo</a>
+<p class="header col s12 white-text">Presenting, the best game ever!</p>
+          </div>
+              <!--<br>-->
+        <!--<br>-->
+      </div>
+  </div>
+
+
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="well well-sm">
+                <form class="form-horizontal" method="post">
+                    <fieldset>
+                        <legend class="text-center header">Contact us</legend>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="fname" name="name" type="text" placeholder="First Name" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="lname" name="name" type="text" placeholder="Last Name" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-envelope-o bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="email" name="email" type="text" placeholder="Email Address" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="phone" name="phone" type="text" placeholder="Phone" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
+                            <div class="col-md-8">
+                                <textarea class="form-control" id="message" name="message" placeholder="Enter your massage for us here. We will get back to you within 2 business days." rows="7"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-12 text-center">
+                                <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .header {
+        color: #36A0FF;
+        font-size: 27px;
+        padding: 10px;
+    }
+
+    .bigicon {
+        font-size: 35px;
+        color: #36A0FF;
+    }
+</style>
+
+
+
+
+<footer class="page-footer orange">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text"> Bio</h5>
+                <p class="grey-text text-lighten-4"> Join oZ5.0 on his grand escape! NüL is the next big, exciting puzzle platformer to hit stores and web consoles to date</p>
+
+
+            </div>
+            <div class="col l3 s12">
+                <h5 class="white-text">Settings</h5>
+                <ul>
+                    <li><a class="white-text" href="https://www.soapui.org/soapui/media/images/stories/Mock/deploy_as_war_dialog_os.png">Subtitles</a>
+                    </li>
+                    <li><a class="white-text" href="https://www.soapui.org/soapui/media/images/stories/Mock/deploy_as_war_dialog_os.png!">Audio</a>
+                    </li>
+                    <li><a class="white-text" href="https://www.soapui.org/soapui/media/images/stories/Mock/deploy_as_war_dialog_os.png">Gameplay</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col l3 s12">
+                <h5 class="white-text">Connect</h5>
+                <ul>
+                    <li><a class="white-text" href="contactus.php">Email Us</a>
+                    </li>
+                    <li><a class="white-text" href="https://github.com/RPappalardo/final-project">Github</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            Brought to you by Nick and Rachel ;)
+        </div>
+    </div>
+</footer>
+
+
+<!--  Scripts-->
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="js/materialize.js"></script>
+<script src="js/init.js"></script>
+
+
+
+</body>
+</html>
