6f64cb55d3734ff27e80baf48e938d0ae2f49c51
RPappalardo <Rachel.Pappalardo001@mymdc.net>
2016-04-19 23:58:23 -0400

    Changed form layout

--- loginpage.php
+++ loginpage.php
@@ -56,9 +56,13 @@
 
     <div class="container white">
         <div class="section">
+
+          <!--makes snaller or large field-->
           <div class="col s12 m4">
               <div class="icon-block">
                   <h5 class="center">
+
+                    <!--login page-->
           <form action="loginpage.php" method="post">
             <fieldset>
               <div class="form-group">
@@ -67,19 +71,29 @@
               <div class="form-group">
                   <input class="form-control" name="password" placeholder="Password" type="password"/>
               </div>
-              <div class="form-group">
-                  <button class="btn btn-default" type="submit">
-                      <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
-                      Log In
-                  </button>
-              </div>
+
             </fieldset>
         <div class="section">
-
+</form>
         </div>
     </div>
 </div>
 
+<div class="col s12 m4">
+        <h5 class="center">
+
+<div class="form-group">
+    <button class="btn btn-default" type="submit">
+        <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
+        Log In
+    </button>
+</div>
+</div>
+</div>
+</div>
+
+
+<!--footer-->
 
     <footer class="page-footer orange">
         <div class="container">
