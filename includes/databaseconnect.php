b3cfce2a29d387b4e9a93c423d38238b13eb448f
nshaheen <nicholas_shaheen@yahoo.com>
2016-04-28 15:20:08 -0400

    corrected db spelling error

--- includes/databaseconnect.php
+++ includes/databaseconnect.php
@@ -2,7 +2,7 @@
   $servername = "localhost";
   $username = "root";
   $password = "";
-  $dbname = "Nul";
+  $dbname = "NuL";
   // Create connection
   $db = mysqli_connect($servername, $username, $password, $dbname);
   // Check connection
