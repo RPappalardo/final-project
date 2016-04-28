3ec68f64c2717d6f8f6cec8386c3c1fb53c288a0
nshaheen <nicholas_shaheen@yahoo.com>
2016-04-28 19:04:13 -0400

    completed styling of registered page

--- registered.php
+++ registered.php
@@ -24,41 +24,81 @@
 
 
 
+<body>
+  <nav class="amber" role="navigation">
+      <div class="nav-wrapper container"><img width="40" src="bigrobologo.jpg">
+          <ul class="right hide-on-med-and-down">
+              <li><a href="index.php">Home</a></li>
+          </ul>
 
-<footer class="page-footer orange">
-    <div class="container">
-        <div class="row">
-            <div class="col l6 s12">
-                <h5 class="white-text"> Bio</h5>
-                <p class="grey-text text-lighten-4"> Join oZ5.0 on his grand escape! NüL is the next big, exciting puzzle platformer to hit stores and web consoles to date</p>
+          <ul id="nav-mobile" class="side-nav">
+            <li><a href="index.php">Home</a>
+            </li>
+            <li><a href="loginpage.php">Login</a>
+            </li>
+            <li><a href="registerpage.php">Register</a>
+            </li>
+            <li><a href="game.php">Demo</a>
+            </li>
+          </ul>
+          <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+      </div>
+  </nav>
+  <div class="section no-pad-bot" id="index-banner">
+      <div class="container">
+          <br>
+          <br>
+          <h1 class="header center orange-text">NüL5.0</h1>
+          <div class="row center">
+              <h3 class ="header col s12 white-text">Thank you for joing us!</h3>
+
+              <h5 class="header col s12 white-text">Presenting, the best game ever!</h5>
+          </div>
+          <div class="row center">
+              <a href="game.php" id="download-button" class="btn-large waves-effect waves-light orange">Demo</a>
+          </div>
+          <br>
+          <br>
+
+      </div>
+  </div>
 
 
-            </div>
-            <div class="col l3 s12">
-                <h5 class="white-text">Settings</h5>
-                <ul>
-                    <li><a class="white-text" href="settings.php">Subtitles</a>
-                    </li>
-                    <li><a class="white-text" href="settings.php">Audio</a>
-                    </li>
-                    <li><a class="white-text" href="settings.php">Gameplay</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col l3 s12">
-                <h5 class="white-text">Connect</h5>
-                <ul>
-                    <li><a class="white-text" href="contactus.php">Email Us</a>
-                    </li>
-                    <li><a class="white-text" href="https://github.com/RPappalardo/final-project">Github</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <div class="footer-copyright">
-        <div class="container">
-            Brought to you by Nick and Rachel ;)
-        </div>
-    </div>
-</footer>
+  <footer class="page-footer orange">
+      <div class="container">
+          <div class="row">
+              <div class="col l6 s12">
+                  <h5 class="white-text"> Bio</h5>
+                  <p class="grey-text text-lighten-4"> Join oZ5.0 on his grand escape! NüL is the next big, exciting puzzle platformer to hit stores and web consoles to date</p>
+
+
+              </div>
+              <div class="col l3 s12">
+                  <h5 class="white-text">Settings</h5>
+                  <ul>
+                      <li><a class="white-text" href="settings.php">Subtitles</a>
+                      </li>
+                      <li><a class="white-text" href="settings.php">Audio</a>
+                      </li>
+                      <li><a class="white-text" href="settings.php">Gameplay</a>
+                      </li>
+                  </ul>
+              </div>
+              <div class="col l3 s12">
+                  <h5 class="white-text">Connect</h5>
+                  <ul>
+                      <li><a class="white-text" href="contactus.php">Email Us</a>
+                      </li>
+                      <li><a class="white-text" href="https://github.com/RPappalardo/final-project">Github</a>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+      <div class="footer-copyright">
+          <div class="container">
+              Brought to you by Nick and Rachel ;)
+          </div>
+      </div>
+  </footer>
+</body>
