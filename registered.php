e2be3228b4c9da61f3d2000e2f05ce2cc22cb616
nshaheen <nicholas_shaheen@yahoo.com>
2016-04-29 15:42:28 -0400

    removed header/footer from registered to test

--- registered.php
+++ registered.php
@@ -1,17 +1,5 @@
 <!DOCTYPE html>
 
-<?php require("includes/databaseconnect.php");
-
-if($_SERVER["REQUEST_METHOD"] == $_POST)
-{
-  if(empty($_POST["user_name"]) || empty($_POST["first_name"]) || empty($_POST["last_name"]) || empty($_POST["email"]) || empty($_POST["password"]))
-  {
-      echo("Please fill the fields.");
-  }
-}
-?>
-
-
   <html lang="en">
 
   <head>
@@ -71,43 +59,4 @@ if($_SERVER["REQUEST_METHOD"] == $_POST)
 
         </div>
     </div>
-
-
-    <footer class="page-footer orange">
-        <div class="container">
-            <div class="row">
-                <div class="col l6 s12">
-                    <h5 class="white-text"> Bio</h5>
-                    <p class="grey-text text-lighten-4"> Join oZ5.0 on his grand escape! NüL is the next big, exciting puzzle platformer to hit stores and web consoles to date</p>
-
-
-                </div>
-                <div class="col l3 s12">
-                    <h5 class="white-text">Settings</h5>
-                    <ul>
-                        <li><a class="white-text" href="settings.php">Subtitles</a>
-                        </li>
-                        <li><a class="white-text" href="settings.php">Audio</a>
-                        </li>
-                        <li><a class="white-text" href="settings.php">Gameplay</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col l3 s12">
-                    <h5 class="white-text">Connect</h5>
-                    <ul>
-                        <li><a class="white-text" href="contactus.php">Email Us</a>
-                        </li>
-                        <li><a class="white-text" href="https://github.com/RPappalardo/final-project">Github</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="footer-copyright">
-            <div class="container">
-                Brought to you by Nick and Rachel ;)
-            </div>
-        </div>
-    </footer>
   </body>
