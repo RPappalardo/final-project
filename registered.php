f16b84c9ba2910e614210082bbe25f378c695040
nshaheen <nicholas_shaheen@yahoo.com>
2016-04-29 17:24:01 -0400

    undid my sillyness

--- registered.php
+++ registered.php
@@ -59,4 +59,42 @@
 
         </div>
     </div>
+    <footer class="page-footer orange">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text"> Bio</h5>
+                    <p class="grey-text text-lighten-4"> Join oZ5.0 on his grand escape! NüL is the next big, exciting puzzle platformer to hit stores and web consoles to date</p>
+
+
+                </div>
+                <div class="col l3 s12">
+                    <h5 class="white-text">Settings</h5>
+                    <ul>
+                        <li><a class="white-text" href="settings.php">Subtitles</a>
+                        </li>
+                        <li><a class="white-text" href="settings.php">Audio</a>
+                        </li>
+                        <li><a class="white-text" href="settings.php">Gameplay</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col l3 s12">
+                    <h5 class="white-text">Connect</h5>
+                    <ul>
+                        <li><a class="white-text" href="contactus.php">Email Us</a>
+                        </li>
+                        <li><a class="white-text" href="https://github.com/RPappalardo/final-project">Github</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                Brought to you by Nick and Rachel ;)
+            </div>
+        </div>
+    </footer>
+
   </body>
