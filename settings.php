90779a351143822dcb9159fd2cc9f98e22ea2252
RPappalardo <Rachel.Pappalardo001@mymdc.net>
2016-04-28 21:11:08 -0400

    Settings page created

---
+++ settings.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+
+<?php require("includes/databaseconnect.php");
+?>
+
+<html lang="en">
+
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+    <title>NüL</title>
+
+    <!-- CSS  -->
+    <link href="css/index.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
+</head>
+
+<body>
+    <nav class="amber" role="navigation">
+        <div class="nav-wrapper container">
+            <ul class="right hide-on-med-and-down">
+                <li>
+                  <a href="index.php">Home</a>
+                </li>
+            </ul>
+
+            <ul id="nav-mobile" class="side-nav">
+                <li><a href="index.php">Home</a>
+                </li>
+                <li><a href="loginpage.php">Login</a>
+                </li>
+                <li><a href="registerpage.php">Register</a>
+                </li>
+                <li><a href="game.php">Demo</a>
+                </li>
+            </ul>
+            <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+        </div>
+    </nav>
+    <div class="section no-pad-bot" id="index-banner">
+        <div class="container grey darken-4">
+            <br>
+            <br>
+
+
+
+
+
+
+
+<p class= "header col s12 white-text" style="text-align: center"> Subtitles </p>
+            <form action="game.php">
+                <p>
+                  <input type="checkbox" id="test1" />
+                  <label for="test1"><a class="white-text">I would like English Subtitles, pls</a></label>
+                </p>
+                <p>
+                  <form action="game.php">
+                      <p>
+                        <input type="checkbox" id="test2" />
+                        <label for="test2"><a class="white-text">I speak another Language</a></label>
+                      </p>
+                      <p>
+<br>
+<br>
+
+<p class= "header col s12 white-text" style="text-align: center"> Audio </p>
+<form action="game.php">
+    <p>
+      <input type="checkbox" id="test3" />
+      <label for="test3"><a class="white-text">Turn that Blasted Noise Down!</a></label>
+    </p>
+    <p>
+      <form action="game.php">
+          <p>
+            <input type="checkbox" id="test4" />
+            <label for="test4"><a class="white-text">I Drive a Car</a></label>
+          </p>
+          <p>
+
+
+<br>
+<br>
+
+<p class= "header col s12 white-text" style="text-align: center"> Game Play </p>
+<form action="game.php">
+    <p>
+      <input type="checkbox" id="test5" />
+      <label for="test5"><a class="white-text">I am a Turtle</a></label>
+    </p>
+    <p>
+      <form action="game.php">
+          <p>
+            <input type="checkbox" id="test6" />
+            <label for="test6"><a class="white-text">I am not a Turtle</a></label>
+          </p>
+          <p>
+
+<center><input type="submit" value="Submit"></center>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <footer class="page-footer orange">
+                <div class="container">
+                    <div class="row">
+                        <div class="col l6 s12">
+                            <h5 class="white-text"> Bio</h5>
+                            <p class="grey-text text-lighten-4"> Join oZ5.0 on his grand escape! NüL is the next big, exciting puzzle platformer to hit stores and web consoles to date</p>
+
+
+                        </div>
+                        <div class="col l3 s12">
+                            <h5 class="white-text">Settings</h5>
+                            <ul>
+                                <li><a class="white-text" href="settings.php">Subtitles</a>
+                                </li>
+                                <li><a class="white-text" href="settings.php">Audio</a>
+                                </li>
+                                <li><a class="white-text" href="settings.php">Gameplay</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col l3 s12">
+                            <h5 class="white-text">Connect</h5>
+                            <ul>
+                                <li><a class="white-text" href="contactus.php">Email Us</a>
+                                </li>
+                                <li><a class="white-text" href="https://github.com/RPappalardo/final-project">Github</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-copyright">
+                    <div class="container">
+                        Brought to you by Nick and Rachel ;)
+                    </div>
+                </div>
+            </footer>
+
+
+            <!--  Scripts-->
+            <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+            <script src="js/materialize.js"></script>
+            <script src="js/init.js"></script>
+
+          </body>
+
+          </html>
