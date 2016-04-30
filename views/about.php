3bd75bb3bbfbae554d0779ce9f6bdecc4c120102
nshaheen <nicholas_shaheen@yahoo.com>
2016-04-29 20:24:19 -0400

    updated css for about/contact

--- views/about.php
+++ views/about.php
@@ -4,12 +4,12 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-<link href="css/index.css" rel="stylesheet">
+<link href="../css/index.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
-<link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
+<link href="../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+<link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
 
-<link href="css/about.css" type="text/css" rel="stylesheet" media="screen,projection" />
+<link href="../css/about.css" type="text/css" rel="stylesheet" media="screen,projection" />
 
 </head>
 
