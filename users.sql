cabac7a209870ec2fd61c303ec3d8337db98d4e1
RPappalardo <Rachel.Pappalardo001@mymdc.net>
2016-05-02 23:07:43 -0400

    Changed it to reflect table

--- users.sql
+++ users.sql
@@ -1,13 +1,13 @@
 
 --
 
-CREATE TABLE `tz_members` (
-  `id` int(11) NOT NULL auto_increment,
-  `usr` varchar(32) collate utf8_unicode_ci NOT NULL default '',
-  `pass` varchar(32) collate utf8_unicode_ci NOT NULL default '',
+CREATE TABLE `nul_users` (
+  `user_id` int(255) NOT NULL auto_increment,
+  `fname` varchar(25) collate utf8_unicode_ci NOT NULL default '',
+  `lname` varchar(25) collate utf8_unicode_ci NOT NULL default '',
   `email` varchar(255) collate utf8_unicode_ci NOT NULL default '',
-  `regIP` varchar(15) collate utf8_unicode_ci NOT NULL default '',
-  `dt` datetime NOT NULL default '0000-00-00 00:00:00',
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `usr` (`usr`)
+  `username` varchar(30) collate utf8_unicode_ci NOT NULL default '',
+  `hash` varchar(255) collate utf8_unicode_ci NOT NULL default '',
+  PRIMARY KEY  (`user_id`),
+  UNIQUE KEY `username` (`username`), `email` (`email`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
