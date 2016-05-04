5883bddab7395326f2a5539f4d3760998c18c1fe
nshaheen <xxnickflick22xx@Nicholass-MacBook-Pro.local>
2016-05-04 17:55:36 -0400

    added scores page

---
+++ views/highscores.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+
+  <html lang="en">
+
+  <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+      <title>NüL</title>
+
+      <!-- CSS  -->
+      <link href="../css/index.css" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link href="../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+      <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
+  </head>
+
+
+
+  <body>
+
+
+    <body>
+        <nav class="amber" role="navigation">
+            <div class="nav-wrapper container">
+                <ul class="right hide-on-med-and-down">
+                    <li>
+                      <a href="../public/index.php">Home</a>
+                    </li>
+                </ul>
+
+                <ul id="nav-mobile" class="side-nav  grey darken-4">
+                  <br>
+                    <li><a href="../public/index.php" class="white-text">Home</a>
+                    </li>
+                    <li><a href="../public/login.php" class="white-text">Login</a>
+                    </li>
+                    <li><a href="../public/register.php" class="white-text">Register</a>
+                    </li>
+                    <li><a href="../views/game.php" class="white-text" >Demo</a>
+                    </li>
+                </ul>
+                <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+            </div>
+        </nav>
+
+    <div class="section no-pad-bot" id="index-banner">
+        <div class="container">
+            <br>
+            <br>
+            <h1 class="header center orange-text">NüL5.0</h1>
+            <div class="row center">
+                <h3 class ="header col s12 white-text">Logged In!</h3>
+
+                <h5 class="header col s12 white-text">Presenting, the best game ever!</h5>
+            </div>
+            <div class="row center">
+                <a href="game.php" id="download-button" class="btn-large waves-effect waves-light orange">Play!</a>
+            </div>
+            <br>
+            <br>
+
+        </div>
+    </div>
+    <footer class="page-footer orange">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text"> Bio</h5>
+                    <p class="grey-text text-lighten-4"> Join oZ5.0 on his grand escape! NüL is the next big, exciting puzzle platformer to hit stores and web consoles to date</p>
+
+
+                </div>
+                <div class="col l3 s12">
+                    <h5 class="white-text">Settings</h5>
+                    <ul>
+                        <li><a class="white-text" href="../public/settings.php">Subtitles</a>
+                        </li>
+                        <li><a class="white-text" href="../public/settings.php">Audio</a>
+                        </li>
+                        <li><a class="white-text" href="../public/settings.php">Gameplay</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col l3 s12">
+                    <h5 class="white-text">Connect</h5>
+                    <ul>
+                        <li><a class="white-text" href="../views/contactus.php">Email Us</a>
+                        </li>
+                        <li><a class="white-text" href="https://github.com/RPappalardo/final-project">Github</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                Brought to you by Nick and Rachel ;)
+            </div>
+        </div>
+    </footer>
+
+  </body>
